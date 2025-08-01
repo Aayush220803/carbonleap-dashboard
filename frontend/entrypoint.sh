824869d8b48bb3bab0f4a6aa25856972df667580
Aayush220802 <aayush.yadav.ug21@nsut.ac.in>
2025-08-01 10:46:47 +0530

    Fix: Recreate entrypoint.sh with correct format

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,4 +1,7 @@
+#!/bin/sh
 
+# Substitute the PORT variable provided by Railway into the Nginx config
 sed -i 's/listen 80;/listen '$PORT';/g' /etc/nginx/conf.d/default.conf
 
+# Start Nginx in the foreground
 exec nginx -g 'daemon off;'
